20c6dc3b1ea193329ecc83c4ad794c466b1ff446
Call Carl <callienicole0419@gmail.com>
2023-02-18 14:15:53 -0600

    added server.js and mysql connection

---
+++ index.js
@@ -0,0 +1,100 @@
+//link to externals
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateDepartments = require('./generateHtml.js');
+const { generateKey } = require('crypto');
+
+//const db = require('./server.js');
+
+const menu = [
+    {
+    type: 'checkbox',
+    name: 'optionmenu',
+    message: 'What would you like to do?',
+    choices: [
+        'View All Departments',
+        'View All Roles',
+        'View All Employees',
+        'Add a Department',
+        'Add a Role',
+        'Add an Employee',
+        'Update an Employee Role'
+    ]
+    }
+];
+
+const addDept = [
+    {
+        type: 'input',
+        name: 'deptname',
+        message: 'Please enter the name of the department:'
+    }
+];
+
+const addRole = [
+    {
+        type: 'input',
+        name: 'rolename',
+        message: 'Please enter the name of the role:'
+    },
+    {
+        type: 'input',
+        name: 'rolesalary',
+        message: 'Please provide the salary:'
+    },
+    {
+        type: 'input',
+        name: 'roledept',
+        message: 'Please provide the department for the role:'
+    }
+];
+
+const addEmployee = [
+    {
+        type: 'input',
+        name: 'employeefirst',
+        message: 'Please provide employee\'s first name:'
+    },
+    {
+        type: 'input',
+        name: 'empployeelast',
+        message: 'Please provide employee\'s last name:'
+    },
+    {
+        type: 'input',
+        name: 'employeerole',
+        message: 'Please enter employee\'s role:'
+    },
+    {
+        type: 'input',
+        name: 'employeemanager',
+        message: 'Please provide employee\'s manager:'
+    }
+    
+];
+
+//b 
+//const updateEmployee = [
+
+//]
+
+
+inquirer.prompt(menu)
+ .then((response) => {
+    fs.writeFile('index.html', generateDepartments(response), () => {
+    if 
+    //(`${response.optionmenu}` === 'View All Departments') {
+        
+    //} else if 
+    (`${response.optionmenu}` === 'Add a Department') {
+        inquirer.prompt(addDept)
+
+    } else if (`${response.optionmenu}` === 'Add a Role') {
+        inquirer.prompt(addRole)
+    } else if (`${response.optionmenu}` === 'Add an Employee') {
+        inquirer.prompt(addEmployee)
+    } else {
+        return;
+    }
+ })
+});
--- server.js
+++ server.js
@@ -1,94 +1,26 @@
-//link to externals
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateDepartments = require('./generateHtml.js');
-const { generateKey } = require('crypto');
+const mysql = require('mysql2');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
-const menu = [
+// Connect to database
+const db = mysql.createConnection(
     {
-    type: 'checkbox',
-    name: 'optionmenu',
-    message: 'What would you like to do?',
-    choices: [
-        'View All Departments',
-        'View All Roles',
-        'View All Employees',
-        'Add a Department',
-        'Add a Role',
-        'Add an Employee',
-        'Update an Employee Role'
-    ]
-    }
-];
-
-const addDept = [
-    {
-        type: 'input',
-        name: 'deptname',
-        message: 'Please enter the name of the department:'
-    }
-];
-
-const addRole = [
-    {
-        type: 'input',
-        name: 'rolename',
-        message: 'Please enter the name of the role:'
+      host: 'localhost',
+      // MySQL username,
+      user: process.env.DB_USER,
+      // MySQL password
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
     },
-    {
-        type: 'input',
-        name: 'rolesalary',
-        message: 'Please provide the salary:'
-    },
-    {
-        type: 'input',
-        name: 'roledept',
-        message: 'Please provide the department for the role:'
-    }
-];
+    console.log(`Connected to the courses_db database.`)
+  );
 
-const addEmployee = [
-    {
-        type: 'input',
-        name: 'employeefirst',
-        message: 'Please provide employee\'s first name:'
-    },
-    {
-        type: 'input',
-        name: 'empployeelast',
-        message: 'Please provide employee\'s last name:'
-    },
-    {
-        type: 'input',
-        name: 'employeerole',
-        message: 'Please enter employee\'s role:'
-    },
-    {
-        type: 'input',
-        name: 'employeemanager',
-        message: 'Please provide employee\'s manager:'
-    }
-    
-];
 
-//b 
-//const updateEmployee = [
-
-//]
-
-inquirer.prompt(menu)
- .then((response) => {
-    fs.writeFile('index.html', generateDepartments(response), () => {
-    if (`${response.optionmenu}` === 'Add a Department') {
-        inquirer.prompt(addDept)
-
-    } else if (`${response.optionmenu}` === 'Add a Role') {
-        inquirer.prompt(addRole)
-    } else if (`${response.optionmenu}` === 'Add an Employee') {
-        inquirer.prompt(addEmployee)
-    } else {
-        return;
-    }
- })
-});
+  module.exports = db;
