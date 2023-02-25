e4669c59a27b45d14303fa5bcaf5945eccfd1167
Call Carl <callienicole0419@gmail.com>
2023-02-25 16:02:45 -0600

    added employee-seeds

---
+++ Develop/seeds/employee-seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles (id, first_name, last_name, title, department, salary, manager)
+VALUES (001, "Bob", "Jones", "cx care representative", "customer care", 25000, "Fred Flinstone"),
+       (002, "Anne", "Frank", "fraud prevention specialist", "fraud prevention", 35000, "George James"),
+       (003, "Herb", "Stanley", "sales specialist", "telesales", 40000, "Earnest Thoompson"),
+       (004, "Brian", "Robinson", "business representative", "business", 35000, "Jordan Belfort")
