742cc81ca925a6a89944c1de95ca7aa1c1d2a0fd
Call Carl <callienicole0419@gmail.com>
2023-02-25 16:10:27 -0600

    updated employee-seeds

--- Develop/seeds/employee-seeds.sql
+++ Develop/seeds/employee-seeds.sql
@@ -1,5 +1,9 @@
 INSERT INTO roles (id, first_name, last_name, title, department, salary, manager)
 VALUES (001, "Bob", "Jones", "cx care representative", "customer care", 25000, "Fred Flinstone"),
        (002, "Anne", "Frank", "fraud prevention specialist", "fraud prevention", 35000, "George James"),
-       (003, "Herb", "Stanley", "sales specialist", "telesales", 40000, "Earnest Thoompson"),
-       (004, "Brian", "Robinson", "business representative", "business", 35000, "Jordan Belfort")
+       (003, "Herb", "Stanley", "sales specialist", "telesales", 40000, "Earnest Thompson"),
+       (004, "Brian", "Robinson", "business representative", "business", 35000, "Jordan Belfort"),
+       (005, "John", "Doe", "cx care supervisor", "customer care", 50000, null),
+       (006, "Mike", "Chan", "fraud prevention supervisor", "fraud prevention", 50000, null),
+       (007, "Tom", "Allen", "sales supervisor", "telesales", 50000, null),
+       (008, "Chris", 'Brown', "buusiness supervisor", "business", 50000, null)
